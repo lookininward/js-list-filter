3048c3e715cd137924fe4b10f4a3afb630da5265
Michael <vinoth.michaelxavier@gmail.com>
2017-03-21 23:33:30 -0400

    successfully passing JSON data from AJAX requests into other functions; now need a way to filter the data before sending back the request response

--- ajax.js
+++ ajax.js
@@ -17,22 +17,31 @@ request.onreadystatechange = function() {
 		var items = JSON.parse(request.responseText);
 		
 		// List
-		var output = '<ul id="teamList">';
-		for (var key in items) {
-			output += '<li><a>' + items[key].name + ' ' + items[key].age + '</a></li>';
-		}
-		output += '</ul>';
+		// var output = '<ul id="teamList">';
+		// for (var key in items) {
+		// 	output += '<li><a>' + items[key].name + ' ' + items[key].age + '</a></li>';
+		// }
+		// output += '</ul>';
 
-		document.getElementById('update').innerHTML = output;
+		// document.getElementById('update').innerHTML = output;
 
-		// Table
-		var output2 = '<table id="teamList2">';
-		for (var key in items) {
-			output2 += '<tr><td>' + items[key].name + ' ' + items[key].age + '</td></tr>';
-		}
-		output2 += '</table>';
+		// // Table
+		// var output2 = '<table id="teamList2">';
+		// for (var key in items) {
+		// 	output2 += '<tr><td>' + items[key].name + ' ' + items[key].age + '</td></tr>';
+		// }
+		// output2 += '</table>';
 
-		document.getElementById('update2').innerHTML = output2;
+		// document.getElementById('update2').innerHTML = output2;
+
+		// List - Alternate
+		// var output3 = '<ul id="teamList3">';
+		// for (var key in items) {
+		// 	output3 += '<li><a>' + items[key].name + ' ' + items[key].age + '</a></li>';
+		// }
+		// output3 += '</ul>';
+
+		// document.getElementById('update3').innerHTML = output3;
 	}
 }
 request.send();
--- filter-list.js
+++ filter-list.js
@@ -1,59 +1,149 @@
-function filterProducts() {
+// document.getElementById('productFilter').onkeyup = filterProducts;
+// document.getElementById('teamFilter').onkeyup = filterTeamMembers;
+// document.getElementById('teamFilterTable').onkeyup = filterTeamMembers_Table;
+document.getElementById('teamFilterAlternate').onkeyup = filterTeamMembersAlternate;
 
-	// Declare variables
-	var input, filter, ul, li, a, i;
-	input = document.getElementById('productFilter');
+// function filterProducts() {
+
+// 	// Declare variables
+// 	var input, filter, ul, li, a, i;
+// 	input = document.getElementById('productFilter');
+// 	filter = input.value.toUpperCase();
+// 	ul = document.getElementById('productList');
+// 	li = ul.getElementsByTagName('li');
+
+// 	// Loop through all list items, hide those who don't match the search query
+// 	for (i = 0; i < li.length; i++) {
+// 		a = li[i].getElementsByTagName('a')[0];
+// 		if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+// 			li[i].style.display = "";
+// 		} else {
+// 			li[i].style.display = "none";
+// 		}
+// 	}
+// };
+
+// function filterTeamMembers() {
+
+// 	// Declare variables
+// 	var input, filter, ul, li, a, i;
+// 	input = document.getElementById('teamFilter');
+// 	filter = input.value.toUpperCase();
+// 	ul = document.getElementById('teamList');
+// 	li = ul.getElementsByTagName('li');
+
+// 	// Loop through all list items, hide those who don't match the search query
+// 	for (i = 0; i < li.length; i++) {
+// 		a = li[i].getElementsByTagName('a')[0];
+// 		if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+// 			li[i].style.display = "";
+// 		} else {
+// 			li[i].style.display = "none";
+// 		}
+// 	}
+// };
+
+// function filterTeamMembers_Table() {
+
+// 	// Declare variables
+// 	var input, filter, table, tr, td, i;
+// 	input = document.getElementById('teamFilterTable');
+// 	filter = input.value.toUpperCase();
+// 	table = document.getElementById('teamList2');
+// 	tr = table.getElementsByTagName('tr');
+
+// 	// Loop through all list items, hide those who don't match the search query
+// 	for (i = 0; i < tr.length; i++) {
+// 		td = tr[i].getElementsByTagName('td')[0];
+// 		if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+// 			tr[i].style.display = "";
+// 		} else {
+// 			tr[i].style.display = "none";
+// 		}
+// 	}
+// };
+
+
+
+// function filterTeamMembersAlternate() {
+
+// 	// Declare variables
+// 	var input, filter, ul, li, a, i;
+// 	input = document.getElementById('teamFilterAlternate');
+// 	filter = input.value.toUpperCase();
+// 	ul = document.getElementById('teamList3');
+// 	li = ul.getElementsByTagName('li');
+
+// 	// Loop through all list items, hide those who don't match the search query
+// 	for (i = 0; i < li.length; i++) {
+// 		a = li[i].getElementsByTagName('a')[0];
+// 		if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+// 			li[i].style.display = "";
+// 		} else {
+// 			li[i].style.display = "none";
+// 		}
+// 	}
+// };
+
+
+var filter = "";
+
+function filterTeamMembersAlternate() {
+	var input;
+	input = document.getElementById('teamFilterAlternate');
 	filter = input.value.toUpperCase();
-	ul = document.getElementById('productList');
-	li = ul.getElementsByTagName('li');
 
-	// Loop through all list items, hide those who don't match the search query
-	for (i = 0; i < li.length; i++) {
-		a = li[i].getElementsByTagName('a')[0];
-		if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-			li[i].style.display = "";
-		} else {
-			li[i].style.display = "none";
+	CreateList();
+}
+
+// Create List
+function CreateList(items) {
+
+		console.log(items[1]);
+		
+
+		output = "<ul id='listOfNames'>";
+		for (var key in items) {
+			output += '<li><a href="#">' + items[key].name + '</a></li>';
+		}
+		output += "</ul>";
+
+		document.getElementById('update').innerHTML = output;
+}
+
+
+// AJAX REQUEST
+// Parse data.json
+function AJAXrequest() {
+	var request;
+
+	// Support for Older Browsers
+	if (window.XMLHttpRequest) {
+		request = new XMLHttpRequest();
+	} else {
+		request = new ActiveXObject('Microsoft.XMLHTTP');
+	}
+
+	// Gather Data
+	request.open('GET', 'json/data.json');
+
+	request.onreadystatechange = function() {
+		if ((request.readyState === 4) && (request.status === 200)) {
+			var items = JSON.parse(request.responseText);
+			console.log(items[1].name);
+			
+			// Pass the parsed items [JSON array] into the CreateList() function
+			CreateList(items);
+
 		}
 	}
-};
 
-function filterTeamMembers() {
+	request.send();
+}
 
-	// Declare variables
-	var input, filter, ul, li, a, i;
-	input = document.getElementById('teamFilter');
-	filter = input.value.toUpperCase();
-	ul = document.getElementById('teamList');
-	li = ul.getElementsByTagName('li');
+AJAXrequest();
 
-	// Loop through all list items, hide those who don't match the search query
-	for (i = 0; i < li.length; i++) {
-		a = li[i].getElementsByTagName('a')[0];
-		if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-			li[i].style.display = "";
-		} else {
-			li[i].style.display = "none";
-		}
-	}
-};
 
-function filterTeamMembers_Table() {
-
-	// Declare variables
-	var input, filter, table, tr, td, i;
-	input = document.getElementById('teamFilterTable');
-	filter = input.value.toUpperCase();
-	table = document.getElementById('teamList2');
-	tr = table.getElementsByTagName('tr');
-
-	// Loop through all list items, hide those who don't match the search query
-	for (i = 0; i < tr.length; i++) {
-		td = tr[i].getElementsByTagName('td')[0];
-		if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-			tr[i].style.display = "";
-		} else {
-			tr[i].style.display = "none";
-		}
-	}
-};
+// Next Step
+// Now the JSON data (all of it) gets parsed and sent out in the request.send,
+// I want it to filter the data when its still inside the ajax request (taking the input); then sent out
