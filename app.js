57fc9667afa859b68a2ce12b8efdc36e45b3411d
Michael <vinoth.michaelxavier@gmail.com>
2017-03-23 18:06:09 -0400

    established connection with mysql database

--- app.js
+++ app.js
@@ -7,3 +7,22 @@ app.use(express.static('public'));
 var server = app.listen(app.get('port'), function() {
 	console.log('Listening on port ' + app.get('port'));
 });
+
+
+// Establish Connection with Database
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host : '127.0.0.1',
+	user : 'root',
+	password : 'l@unchdetected!',
+	database : 'my_db'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(error, results, fields) {
+	if (error) throw error;
+	console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
